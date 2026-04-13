afe127276ddda1c922a21afec3215041b589658c
Javierchapi <javchagar@alum.us.es>
2026-04-13 16:25:22 +0200

    Eliminamos el comentario de la prueba

--- src/main/java/aiss/videominer/VideominerApplication.java
+++ src/main/java/aiss/videominer/VideominerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VideominerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VideominerApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(VideominerApplication.class, args);
 	}
 
 }
