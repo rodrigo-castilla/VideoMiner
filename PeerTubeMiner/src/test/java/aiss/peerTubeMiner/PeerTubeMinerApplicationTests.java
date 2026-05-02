4fe4309f1efa4b64cdbc29dc0a8c47c4efb37dea
davdursaa <davdursaa@alum.us.es>
2026-05-02 17:30:09 +0200

    PeerTubeMiner tests

---
+++ PeerTubeMiner/src/test/java/aiss/peerTubeMiner/PeerTubeMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.peertubeminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerTubeMinerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
