ed76cbf5676512c442ac02913429ea6a31947646
Javierchapi <javchagar@alum.us.es>
2026-05-08 12:18:29 +0200

    Refactoring some errors

--- PeerTubeMiner/src/main/java/aiss/peertubeminer/controller/PeerTubeController.java
+++ PeerTubeMiner/src/main/java/aiss/peertubeminer/controller/PeerTubeController.java
@@ -2,7 +2,8 @@ package aiss.peertubeminer.controller;
 
 import aiss.peertubeminer.service.PeerTubeService;
 import aiss.peertubeminer.service.VideoMinerService;
-import org.springframework.http.HttpStatus;
+import aiss.peertubeminer.model.videominer.Channel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,13 +24,8 @@ public class PeerTubeController {
             @RequestParam(defaultValue = "10") int maxVideos,
             @RequestParam(defaultValue = "10") int maxComments) {
 
-        // 1. Descarga los datos de PeerTube
         Channel channel = peerTubeService.getChannel(channelId, maxVideos, maxComments);
-
-        // 2. Envía los datos al VideoMiner (puerto 8080)
         videoMinerService.sendChannelToVideoMiner(channel);
-
-
         return channel;
     }
 }
